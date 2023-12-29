42b444830c24790959f8863aa3d707cfc9a4cf33
frclasso <frclasso@yahoo.com.br>
2023-12-29 15:33:18 -0300

    sextou

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html')
     
 def sobre(request):
     return HttpResponse("About us")
