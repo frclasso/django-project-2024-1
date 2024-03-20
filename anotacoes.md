816896bf7f43f4bf5ad7e93b7c6304410f40ea47
Fabio Classo <frclasso@yahoo.com.br>
2024-03-20 18:10:53 -0300

    starting coverage

--- anotacoes.md
+++ anotacoes.md
@@ -1,1 +1,14 @@
 # Anotacoes
+
+- run pytest django on terminal
+ python .\manage.py test  
+
+
+- run coverage
+coverage run --omit='*/venv/*' -m pytests
+coverage run  -m pytest
+
+- run coverage on django
+coverage run manage.py test
+
+
--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,6 +1,7 @@
 
-from .test_recipe_base import RecipeTestBase
+from .test_recipe_base import RecipeTestBase, Recipe
 from django.core.exceptions import ValidationError
+from parameterized import parameterized
 
 
 class RecipeModelTest(RecipeTestBase):
@@ -8,9 +9,42 @@ class RecipeModelTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
     
-    def test_recipe_title_length_is_greater_than_65_chars(self):
-        self.recipe.title = "Um valor qualquer" * 5
+    def make_recipce_no_defaults(self):
+        recipe = Recipe(
+            category=self.make_category(name='Default Category test'),
+            author=self.make_author(username='New User'),
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps'
+        )
+        recipe.full_clean()
+        recipe.save()
+        return recipe
+    # def test_recipe_title_length_is_greater_than_65_chars(self):
+    #     self.recipe.title = "Um valor qualquer" * 5
 
+    #     with self.assertRaises(ValidationError):
+    #         self.recipe.full_clean() # Faz a validacao e quebra  ValidationError
+
+    @parameterized.expand([('title', 65),('description', 165),('preparation_time_unit', 65),('servings_unit', 65)])
+    def test_recipe_fields_max_length(self, field, max_length):
+        setattr(self.recipe, field, 'A' * (max_length + 1)) # selecar 0, ou seja, nao alterar o length, falha
         with self.assertRaises(ValidationError):
-            self.recipe.full_clean() # Faz a validacao e quebra  ValidationError
+            self.recipe.full_clean()
 
+    def test_recipe_preparation_steps_is_html_is_false_by_default(self):
+        '''Alterando no model pra True, falhas'''
+        recipe = self.make_recipce_no_defaults()
+        self.assertFalse(recipe.preparation_steps_is_html,
+                        msg='Recipe preparation_steps_is_html is True by default.') 
+        
+    def test_recipe_is_published_by_default(self):
+        '''Alterando no model pra True, falhas'''
+        recipe = self.make_recipce_no_defaults()
+        self.assertFalse(recipe.is_published,
+                        msg='Recipe is_published is True by default.')
