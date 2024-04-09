560f0207e44e5de5221649123b2da5abb5288f45
Fabio Classo <frclasso@yahoo.com.br>
2024-04-09 11:56:21 -0300

    test_recipe_search_can_find_recipe_by_title test function

--- recipes/views.py
+++ recipes/views.py
@@ -46,9 +46,11 @@ def search(request):
         raise Http404()
     
     recipes = Recipe.objects.filter(
-        Q(title__icontains=search_term) |Q(description__icontains=search_term),
-
-    ).order_by('-id')
+                                        Q(
+                                            Q(title__icontains=search_term) |Q(description__icontains=search_term),
+                                            is_published=True
+                                        )
+                                    ).order_by('-id')
 
     return render(request, 'recipes/pages/search.html',
                   {
