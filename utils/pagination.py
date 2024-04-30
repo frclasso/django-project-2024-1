09a73957a0078d9ea3d6307cbf5c68f567b78c21
Fabio Classo <frclasso@yahoo.com.br>
2024-04-30 17:30:25 -0300

    start devlopment

---
+++ utils/pagination.py
@@ -0,0 +1,5 @@
+def make_pagination_range(
+        page_range,
+        qtd_paginas,
+        current_page):
+    return [1,2,3,4]
--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -1,12 +1,15 @@
 from unittest import TestCase
+from pagination import make_pagination_range
 
 class PaginationTest(TestCase):
-
+    # red, green e refactor
     def test_make_pagination_range_returns_pagination(self):
 
         pagination = make_pagination_range(
             page_range = list(range(1,12)),
-            qtd_paginas = 4
+            qtd_paginas = 4,
             current_page=1
         )
-        self.assertIn([1,2,3,4], pagination)
+        self.assertEqual([1,2,3,4], pagination)
+
+   
